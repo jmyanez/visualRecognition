923ee5046cfdfa5c5bc94fe1a350f3a8c83ac036
jmyanez <jmyanez@miners.utep.edu>
2018-04-29 02:12:48 -0600

    rffdf

--- visualTest.py
+++ visualTest.py
@@ -5,10 +5,11 @@ visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     api_key='3995db77bd70aa498157544096c84c5741b30668')
 
-with open('./dog.jpg', 'rb') as images_file:
-    classes = visual_recognition.classify(
-        images_file,
-        parameters = json.dumps({
-            'classifier_ids': ["dogs_908544050"]
-        }))
-print(json.dumps(classes, indent=2))#
+with open('./pata.zip', 'rb') as human,open('pata2.zip','rb')as human2\
+        , open('./cats.zip', 'rb') as cats:
+    model = visual_recognition.create_classifier(
+        'humans',
+        human_positive_examples=human,
+        human1_positive_examples = human2,
+        negative_examples=cats)
+print(json.dumps(model, indent=2))
