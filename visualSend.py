abb8d3a65d05676f9dad15d4a7affd90795f6ecb
Daniel Ornelas <dornelas4@miners.utep.edu>
2018-04-29 04:48:18 -0600

    dd

--- visualSend.py
+++ visualSend.py
@@ -1,7 +1,7 @@
 import json
 from watson_developer_cloud import VisualRecognitionV3
 from Xlib import display, X
-from pillow import Image #PIL
+from PIL import Image #PIL
 import time
 
 print("HE")
@@ -15,14 +15,14 @@ dsp = display.Display()
 root = dsp.screen().root
 x = 0
 
+
 while x < 2:
     raw = root.get_image(200, 200, W, H, X.ZPixmap, 0xffffffff)
     image = Image.frombytes("RGB", (W, H), raw.data, "raw", "BGRX")
-    image.show()
     time.sleep(.5)
-    x += 1
     classes = visual_recognition.classify(
         image,
         threshold='0.6',
         classifier_ids='humans_1546902740')
+    x += 1
     print(json.dumps(classes, indent=2))
