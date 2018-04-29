4ae84e781110002108217af28f6ea1cb7d8c6603
jmyanez <jmyanez@miners.utep.edu>
2018-04-29 02:41:20 -0600

    reads filename from command prompt

--- visualSend.py
+++ visualSend.py
@@ -5,7 +5,11 @@ visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     api_key='3995db77bd70aa498157544096c84c5741b30668')
 
-with open('./apie.jpg', 'rb') as images_file:
+
+filename = raw_input("Some input please: ")
+
+
+with open(filename, 'rb') as images_file:
     classes = visual_recognition.classify(
         images_file,
         threshold='0.6',
