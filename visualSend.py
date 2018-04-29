36ca8d94036b8eac511a97c73631b023da62406f
jmyanez <jmyanez@miners.utep.edu>
2018-04-29 02:29:53 -0600

    rffdf

---
+++ visualSend.py
@@ -0,0 +1,13 @@
+import json
+from watson_developer_cloud import VisualRecognitionV3
+
+visual_recognition = VisualRecognitionV3(
+    '2018-03-19',
+    api_key='3995db77bd70aa498157544096c84c5741b30668')
+
+with open('./apie.jpg', 'rb') as images_file:
+    classes = visual_recognition.classify(
+        images_file,
+        threshold='0.6',
+        classifier_ids='humans_1546902740')
+    print(json.dumps(classes, indent=2))
